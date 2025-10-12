e3cbfc3b9d2aeee40ce21dd9f7c52ac9dc641bb0
pauxav06 <paul.heibig@epfl.ch>
2025-10-12 11:50:16 +0200

    feat(nnue): added utils for Google Highway layers integration

---
+++ engine/include/ChePP/nnue/utils.h
@@ -0,0 +1,25 @@
+#ifndef CHEPP_NNUE_UTILS_H_
+#define CHEPP_NNUE_UTILS_H_
+
+#include <hwy/aligned_allocator.h>
+
+#include <array>
+#include <cstddef>
+#include <mdspan/mdspan.hpp>
+
+namespace chepp::nnue::utils
+{
+    constexpr int pad_up(const int n, const int m) noexcept
+    {
+        return ((n + m - 1) / m) * m;
+    }
+
+    constexpr int pad_down(const int x, const int n) noexcept
+    {
+        if (n == 0)
+            return x;
+        return (x >= 0) ? (x / n) * n : ((x - n + 1) / n) * n;
+    }
+} // namespace chepp::nnue::utils
+
+#endif
