aeb55b5f9680468c7ccc32880f7997a049c54279
pauxav06 <paul.heibig@epfl.ch>
2025-10-13 23:01:00 +0200

    feat(nnue): random utils added for testing / tune

--- engine/include/ChePP/nnue/utils.h
+++ engine/include/ChePP/nnue/utils.h
@@ -21,6 +21,32 @@ namespace chepp::nnue::utils
             return x;
         return (x >= 0) ? (x / n) * n : ((x - n + 1) / n) * n;
     }
+
+    template <typename T>
+   void fill_random(
+       T* ptr,
+       const size_t n,
+       const T min = std::numeric_limits<T>::min(),
+       const T max = std::numeric_limits<T>::max(),
+       const unsigned seed = 1234)
+    {
+        std::mt19937                       rng(seed);
+        std::uniform_int_distribution dist(min, max);
+        for (size_t i = 0; i < n; ++i)
+        {
+            ptr[i] = static_cast<T>(dist(rng));
+        }
+    }
+
+    template <typename T, typename Alloc>
+    void fill_random(
+        std::vector<T, Alloc>& v,
+        const T min = std::numeric_limits<T>::min(),
+        const T max = std::numeric_limits<T>::max(),
+        unsigned seed = 1234)
+    {
+        fill_random(v.data(), v.size(), min, max, seed);
+    }
 } // namespace chepp::nnue::utils
 
 #endif
