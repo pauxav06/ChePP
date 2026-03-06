25bb6a314d30da2f6be0e1099f9863ebc96e787e
pauxav06 <paul.heibig@epfl.ch>
2026-03-06 20:16:56 +0100

    feat(main): test for second release

--- engine/src/nnue/weights/weights.h
+++ engine/src/nnue/weights/weights.h
@@ -3,7 +3,7 @@
 #include <cstdint>
 
 #if defined(IDE) && IDE == 1
-inline constexpr std::array<uint8_t, 47128384> GENERATED_WEIGHTS{};
+inline constinit const std::array<uint8_t, 47128384> GENERATED_WEIGHTS{};
 #else
 #include "/home/paul/code/ChePP/engine/src/nnue/weights/weights.inc"
 #endif
